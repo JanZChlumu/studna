3e579aa906b32744c205e0d4ba1c8036d98ae028
Jan Vladyka <jan.vladyka@siemens.com>
2025-04-03 08:56:34 +0200

    uklid

--- .not2Pico/micropython-lcd12864-main/tools/preved_pismo.bat
+++ .not2Pico/micropython-lcd12864-main/tools/preved_pismo.bat
@@ -1,6 +1,6 @@
 @echo off
 
-c:\Users\Z004NBWP\tools\thonny-4.1.7-windows-portable\python.exe c:\Users\Z004NBWP\Git\MicroPythonTest\.not2Pico\micropython-lcd12864-main\tools\font_to_py.py -x c:\Windows\Fonts\BKANT.TTF 70  BookAntiqua70CZ.py -k c:\Users\Z004NBWP\Git\MicroPythonTest\.not2Pico\micropython-lcd12864-main\tools\czech 
+c:\Users\Z004NBWP\tools\thonny-4.1.7-windows-portable\python.exe c:\Users\Z004NBWP\Git\MicroPythonTest\.not2Pico\micropython-lcd12864-main\tools\font_to_py.py -x c:\Windows\Fonts\BKANT.TTF 14 BookAntiqua14CZV2.py -k c:\Users\Z004NBWP\Git\MicroPythonTest\.not2Pico\micropython-lcd12864-main\tools\czech 
 
 
 echo Hotovo!
